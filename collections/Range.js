7a94867aef41ee4c6f7f74e659f826406b64c283
Derk Norton <derk.norton@gmail.com>
2018-06-24 12:18:03 -0600

    Added unit tests for the stack class and created a new range collection class.

---
+++ collections/Range.js
@@ -0,0 +1,134 @@
+/************************************************************************
+ * Copyright (c) Crater Dog Technologies(TM).  All Rights Reserved.     *
+ ************************************************************************
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.        *
+ *                                                                      *
+ * This code is free software; you can redistribute it and/or modify it *
+ * under the terms of The MIT License (MIT), as published by the Open   *
+ * Source Initiative. (See http://opensource.org/licenses/MIT)          *
+ ************************************************************************/
+'use strict';
+
+/**
+ * This collection class extracts a range of items from a collection data structure. It then
+ * provides an iterator to iterate over the range of items. A range, once formed, remains
+ * static.
+ */
+var Composite = require('./Composite').Composite;
+
+
+/**
+ * The constructor for the range class takes a first and last item and an optional
+ * collection as arguments. If no collection is provided the range is assumed to be
+ * over the collection of all integers.
+ * 
+ * @param {Number} first The first item in the range.
+ * @param {Number} last The last item in the range.
+ * @param {Collection} optionalCollection The collection that the items belong to.
+ */
+function Range(first, last, optionalCollection) {
+    Composite.call(this);
+    this.first = first;
+    this.last = last;
+    if (optionalCollection) {
+        this.array = [];
+        var iterator = optionalCollection.iterator();
+        var inRange = false;
+        while (iterator.hasNext()) {
+            var item = iterator.getNext();
+            if (item.equalTo(first)) inRange = true;
+            if (inRange) this.array.push(item);
+            if (item.equalTo(last)) {
+                inRange = false;
+                break;
+            }
+        }
+    } else if (!Number.isInteger(first) || !Number.isInteger(last)) {
+        throw new Error('A number range must have first and last items that are integers.');
+    }
+    return this;
+}
+Range.prototype = Object.create(Composite.prototype);
+Range.prototype.constructor = Range;
+exports.Range = Range;
+
+
+// PUBLIC METHODS
+
+/**
+/**
+ * This method returns the number of items that are in the range.
+ * 
+ * @returns {Number}
+ */
+Range.prototype.getSize = function() {
+    var size = this.array ? this.array.length : this.last - this.first + 1;
+    return size;
+};
+
+
+/**
+ * This method creates an iterator that can be used to iterate over the range.
+ * 
+ * @returns {Iterator}
+ */
+Range.prototype.iterator = function() {
+    var iterator = new RangeIterator(this);
+    return iterator;
+};
+
+
+// PRIVATE CLASSES
+
+/**
+ * The constructor for the RangeIterator class.
+ * 
+ * @param {Range} range The range to be iterated over.
+ * @returns {RangeIterator} The new range iterator.
+ */
+function RangeIterator(range) {
+    this.slot = 0;  // the slot before the first item
+    this.size = range.getSize();  // static so we can cache it here
+    this.range = range;
+    return this;
+}
+RangeIterator.prototype.constructor = RangeIterator;
+
+
+RangeIterator.prototype.toStart = function() {
+    this.slot = 0;  // the slot before the first item
+};
+
+
+RangeIterator.prototype.toSlot = function(slot) {
+    this.slot = slot;
+};
+
+
+RangeIterator.prototype.toEnd = function() {
+    this.slot = this.size;  // the slot after the last item
+};
+
+
+RangeIterator.prototype.hasPrevious = function() {
+    return this.slot > 0;
+};
+
+
+RangeIterator.prototype.hasNext = function() {
+    return this.slot < this.size;
+};
+
+
+RangeIterator.prototype.getPrevious = function() {
+    if (!this.hasPrevious()) throw new Error("The iterator is at the beginning of the range.");
+    var item = this.range.array[--this.slot];
+    return item;
+};
+
+
+RangeIterator.prototype.getNext = function() {
+    if (!this.hasNext()) throw new Error("The iterator is at the end of the range.");
+    var item = this.range.array[this.slot++];
+    return item;
+};
---
+++ test/collections/TestStack.js
@@ -0,0 +1,161 @@
+/************************************************************************
+ * Copyright (c) Crater Dog Technologies(TM).  All Rights Reserved.     *
+ ************************************************************************
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.        *
+ *                                                                      *
+ * This code is free software; you can redistribute it and/or modify it *
+ * under the terms of The MIT License (MIT), as published by the Open   *
+ * Source Initiative. (See http://opensource.org/licenses/MIT)          *
+ ************************************************************************/
+
+var collections = require('../../collections/');
+var mocha = require('mocha');
+var expect = require('chai').expect;
+
+
+describe('Bali Virtual Machine™', function() {
+
+    describe('Test the stack constructors.', function() {
+
+        it('should create a stack with default capacity', function() {
+            var stack = new collections.Stack();
+            expect(stack).to.exist;  // jshint ignore:line
+            var size = stack.getSize();
+            expect(size).to.exist;  // jshint ignore:line
+            expect(size).to.equal(0);
+            var iterator = stack.iterator();
+            expect(iterator).to.exist;  // jshint ignore:line
+            expect(iterator.hasNext() === false);
+            expect(iterator.hasPrevious() === false);
+            stack.removeAll();
+        });
+
+        it('should create a stack with small capacity', function() {
+            var stack = new collections.Stack(1);
+            var size = stack.getSize();
+            expect(size).to.exist;  // jshint ignore:line
+            expect(size).to.equal(0);
+            var item = new Item(1);
+            stack.pushItem(item);
+            size = stack.getSize();
+            expect(size).to.exist;  // jshint ignore:line
+            expect(size).to.equal(1);
+            expect(function() {stack.pushItem(item);}).to.throw(Error, "Attempted to push an item onto a full stack.");
+            var top = stack.popItem();
+            expect(top).to.exist;  // jshint ignore:line
+            expect(item.equalTo(top)).to.equal(true);
+            size = stack.getSize();
+            expect(size).to.exist;  // jshint ignore:line
+            expect(size).to.equal(0);
+        });
+
+        it('should be able to push and pop items from a stack', function() {
+            var stack = new collections.Stack();
+            var item1 = new Item(1);
+            var item2 = new Item(2);
+            var item3 = new Item(3);
+            var item4 = new Item(4);
+            var item5 = new Item(5);
+            stack.pushItem(item2);
+            stack.pushItem(item5);
+            stack.pushItem(item3);
+            stack.pushItem(item1);
+            stack.pushItem(item4);
+            var size = stack.getSize();
+            expect(size).to.exist;  // jshint ignore:line
+            expect(size).to.equal(5);
+            var top = stack.getTop();
+            expect(item4.equalTo(top)).to.equal(true);
+            var pop = stack.popItem();
+            expect(top.equalTo(pop)).to.equal(true);
+            expect(stack.getSize()).to.equal(4);
+            top = stack.getTop();
+            expect(item1.equalTo(top)).to.equal(true);
+            pop = stack.popItem();
+            expect(top.equalTo(pop)).to.equal(true);
+            expect(stack.getSize()).to.equal(3);
+            top = stack.getTop();
+            expect(item3.equalTo(top)).to.equal(true);
+            pop = stack.popItem();
+            expect(top.equalTo(pop)).to.equal(true);
+            expect(stack.getSize()).to.equal(2);
+            top = stack.getTop();
+            expect(item5.equalTo(top)).to.equal(true);
+            pop = stack.popItem();
+            expect(top.equalTo(pop)).to.equal(true);
+            expect(stack.getSize()).to.equal(1);
+            top = stack.getTop();
+            expect(item2.equalTo(top)).to.equal(true);
+            pop = stack.popItem();
+            expect(top.equalTo(pop)).to.equal(true);
+            expect(stack.getSize()).to.equal(0);
+        });
+
+        it('should iterate over a stack forwards and backwards', function() {
+            // REMEMBER: The iterator for a stack iterates through the items in LIFO order
+            var stack = new collections.Stack();
+            var items = [
+                new Item(0),
+                new Item(1),
+                new Item(2),
+                new Item(3),
+                new Item(4)
+            ];
+            var index;
+            var item;
+            // place the items on the stack in order
+            while (index < items.length) {
+                stack.pushItem(items[index++]);
+            }
+            // iterate through the items from top down
+            var iterator = stack.iterator();
+            expect(iterator).to.exist;  // jshint ignore:line
+            expect(iterator.hasPrevious() === false);
+            expect(iterator.hasNext() === true);
+            while (index > 0) {  // now go through in LIFO order
+                item = iterator.getNext();
+                expect(items[index--].equalTo(item)).to.equal(true);
+            }
+            // should be at the last slot in the iterator
+            expect(iterator.hasPrevious() === true);
+            expect(iterator.hasNext() === false);
+            // move to the first slot in the iterator
+            iterator.toStart();
+            expect(iterator.hasPrevious() === false);
+            expect(iterator.hasNext() === true);
+            // move back to the last slot in the iterator
+            iterator.toEnd();
+            expect(iterator.hasPrevious() === true);
+            expect(iterator.hasNext() === false);
+            // iterator through the items from bottom up
+            while (index < items.length) {
+                item = iterator.getPrevious();
+                expect(items[index++].equalTo(item)).to.equal(true);
+            }
+            // should be at the first slot in the iterator
+            expect(iterator.hasPrevious() === false);
+            expect(iterator.hasNext() === true);
+        });
+
+    });
+
+});
+
+function Item(value) {
+    this.value = value;
+    return this;
+}
+Item.prototype.constructor = Item;
+
+
+Item.prototype.equalTo = function(that) {
+    return this.value === that.value;
+};
+
+
+Item.prototype.compareTo = function(that) {
+    if (this.value < that.value) return -1;
+    if (this.value === that.value) return 0;
+    if (this.value > that.value) return 1;
+};
+
